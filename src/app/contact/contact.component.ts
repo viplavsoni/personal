04210555ffb0a771bf17d16f6c387882a3640114
unknown <viplavsoni3333@gmail.com>
2021-03-15 15:25:25 +0530

    Modified the contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  public contactInfo = [
+    {contactNumber: "7869600848"},
+    {email: "viplavsoni3333@gmail.com"},
+    {address: "Raipur ( C.G. )"},
+    {githubLink: "https://github.com/viplavsoni"},
+    {stackOverflow: "https://stackoverflow.com/users/11727315/viplav-soni"},
+    {twitter: "https://twitter.com/viplav_soni"},
+    {youtube: " https://youtube.com/technicalviplav"}
+  ];
+  objectKeys = Object.keys;
   constructor() { }
 
   ngOnInit(): void {
+    this.loadIntialData();
+  }
+
+  loadIntialData() {
+    let contactInfo = JSON.parse(localStorage.contactInfo);
+    this.contactInfo = contactInfo? contactInfo: this.contactInfo; 
   }
 
 }
